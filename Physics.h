4b4987f7d0adae13d517b83c8878ff961480b7c3
Gerardo Garcia Lopez <gerardoagl96@gmail.com>
2025-06-14 12:36:30 -0500

    Sensors working fine

--- Physics.h
+++ Physics.h
@@ -11,14 +11,21 @@ class Physics {
     
     bool areColliding (Entity* a, Entity* b) const;
 
-    float   GetTileHeight           (const Vec2& sensor, Entity* tile) const;
-    float   GetTileAngleForPlayer   (Entity* player, Entity* tile);
-    Entity* GetTileForBottomSensor  (const Vec2& sensor, std::map<unsigned int, Entity*>& map);
-    float   GetTileVerticalDistance (const Vec2& sensor, Entity* tile) const;
-    float   GetLeftSensorOverlap    (const Vec2& sensor, Entity* tile) const;
-    float   GetRightSensorOverlap   (const Vec2& sensor, Entity* tile) const;
-    float   GetTopSensorOverlap     (const Vec2& sensor, Entity* tile) const;
-    float   GetBottomSensorOverlap  (const Vec2& sensor, Entity* tile) const;
+    float   GetTileHeight            (const Vec2& sensor, Entity* tile) const;
+    float   GetTileWidth             (const Vec2& sensor, Entity* tile) const;
+    Entity* GetTileForBottomSensor   (const Vec2& sensor, std::map<unsigned int, Entity*>& map);
+    Entity* GetTileForTopSensor      (const Vec2& sensor, std::map<unsigned int, Entity*>& map);
+    Entity* GetTileForRightSensor    (const Vec2& sensor, std::map<unsigned int, Entity*>& map);
+    Entity* GetTileForLeftSensor     (const Vec2& sensor, std::map<unsigned int, Entity*>& map);
+    float   GetTileAngleForPlayer    (Entity* player, Entity* tile);
+    float   GetTileVerticalDistance  (const Vec2& sensor, Entity* tile) const;
+    float   GetTileDistanceFromTop   (const Vec2& sensor, Entity* tile) const;
+    float   GetTileDistanceFromLeft  (const Vec2& sensor, Entity* tile) const;
+    float   GetTileDistanceFromRight (const Vec2& sensor, Entity* tile) const;
+    float   GetLeftSensorOverlap     (const Vec2& sensor, Entity* tile) const;
+    float   GetRightSensorOverlap    (const Vec2& sensor, Entity* tile) const;
+    float   GetTopSensorOverlap      (const Vec2& sensor, Entity* tile) const;
+    float   GetBottomSensorOverlap   (const Vec2& sensor, Entity* tile) const;
 };
 
 #endif
